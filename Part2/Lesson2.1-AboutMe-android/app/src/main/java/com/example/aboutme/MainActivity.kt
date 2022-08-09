a1c4550d0fa5780eb772aabe358d2681721c1685
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-08-09 22:43:35 +0200

    part2

---
+++ Part2/Lesson2.1-AboutMe-android/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private val myName:MyName = MyName("Aleks Haecky")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        findViewById<Button>(R.id.done_button).setOnClickListener{
+//            addNickname(it)
+//        }
+        binding.myName = myName
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+    private fun addNickname(view: View){
+        binding.apply {
+//            nicknameText.text=binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ Part2/Lesson2.2-ColorMyViews-android/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val boxOne = findViewById<TextView>(R.id.box_one_text)
+        val boxTwo = findViewById<TextView>(R.id.box_two_text)
+        val boxThree = findViewById<TextView>(R.id.box_three_text)
+        val boxFour = findViewById<TextView>(R.id.box_four_text)
+        val boxFive = findViewById<TextView>(R.id.box_five_text)
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.constraint_layout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        val clickableViews: List<View> =
+            listOf(boxOne, boxTwo, boxThree, boxFour, boxFive, constraintLayout, redButton, greenButton, yellowButton)
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it, boxThree, boxFour, boxFive) }
+        }
+    }
+
+    private fun makeColored(view: View, boxThree:TextView, boxFour:TextView, boxFive:TextView) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> boxThree.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFour.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFive.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
