565f525b5d398c2bbaefb6736af8db6578fd1e0b
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-08-16 14:30:46 +0200

    span of header = 2

--- Part2/Lesson2-SleepTrackerRecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Part2/Lesson2-SleepTrackerRecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -114,7 +114,7 @@ class SleepTrackerFragment : Fragment() {
 
         manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
             override fun getSpanSize(position: Int) =  when (position) {
-                0 -> 3
+                0 -> 2
                 else -> 1
             }
         }
