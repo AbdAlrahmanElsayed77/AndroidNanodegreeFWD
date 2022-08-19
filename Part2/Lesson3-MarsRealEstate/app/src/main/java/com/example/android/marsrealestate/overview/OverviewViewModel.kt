12258e07737f002d8f0fba4060f3e772265dbfa9
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-08-19 15:05:15 +0200

    Part2Lesson3 Done

--- Part2/Lesson3-MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Part2/Lesson3-MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiFilter
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
 
@@ -55,17 +56,17 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
     }
 
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
         viewModelScope.launch {
             _status.value = MarsApiStatus.LOADING
             try {
-                var listResult: List<MarsProperty> = MarsApi.retrofitService.getProperties()
+                var listResult: List<MarsProperty> = MarsApi.retrofitService.getProperties(filter.value)
                 _properties.value = listResult
                 _status.value = MarsApiStatus.DONE
             } catch (e: Exception) {
@@ -79,5 +80,8 @@ class OverviewViewModel : ViewModel() {
     }
     fun displayPropertyDetailsComplete() {
         _navigateToSelectedProperty.value = null
+    }
+    fun updateFilter(filter: MarsApiFilter) {
+        getMarsRealEstateProperties(filter)
     }
 }
