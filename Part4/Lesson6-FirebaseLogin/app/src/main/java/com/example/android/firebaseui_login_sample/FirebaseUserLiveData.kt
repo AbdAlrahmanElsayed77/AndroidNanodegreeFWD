230188459f1404994cb2d933e2dd0db4fc3b8410
Adel <a.adel.a.fattah@gmail.com>
2022-09-20 18:11:27 +0200

    lesson 6

--- Part4/Lesson6-FirebaseLogin/app/src/main/java/com/example/android/firebaseui_login_sample/FirebaseUserLiveData.kt
+++ Part4/Lesson6-FirebaseLogin/app/src/main/java/com/example/android/firebaseui_login_sample/FirebaseUserLiveData.kt
@@ -26,19 +26,13 @@ import androidx.lifecycle.LiveData
  *
  * Note that onActive() and onInactive() will get triggered when the configuration changes (for
  * example when the device is rotated). This may be undesirable or expensive depending on the
- * nature of your LiveData object, but is okay for this purpose since we are only adding and
- * removing the authStateListener.
+ * nature of your LiveData object.
  */
 class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
     private val firebaseAuth = FirebaseAuth.getInstance()
 
-    // TODO set the value of this FireUserLiveData object by hooking it up to equal the value of the
-    //  current FirebaseUser. You can utilize the FirebaseAuth.AuthStateListener callback to get
-    //  updates on the current Firebase user logged into the app.
     private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
-        // TODO Use the FirebaseAuth instance instantiated at the beginning of the class to get an
-        //  entry point into the Firebase Authentication SDK the app is using.
-        //  With an instance of the FirebaseAuth class, you can now query for the current user.
+        value = firebaseAuth.currentUser
     }
 
     // When this object has an active observer, start observing the FirebaseAuth state to see if
